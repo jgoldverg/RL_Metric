c6a4e1db3c77024cac6626343f2f9b130627a049
Elvis Rodrigues <elvis@elrodrigues.com>
2022-04-14 01:13:06 -0400

    action space clarification needed

--- codes/netEnv.py
+++ codes/netEnv.py
@@ -5,9 +5,10 @@ import numpy as np
 class NetEnvironment(gym.Env):
     metadata = {'render.modes': []}
     def __init__(self):
-        # unbounded box for now
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(5,), dtype=np.float32)
-        self.action_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.int16)
+        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(5,), dtype=np.float32)
+        # clarification needed in defining action space: discrete?
+        # fix a set of actions and pass a 'batch' of state with actions to net?
+        self.action_space = spaces.Box(low=0, high=np.inf, shape=(3,), dtype=np.int16)
     def reset(self):
         pass
     def step(self, observation):
